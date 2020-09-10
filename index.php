0a764ca295515982188c7353a6394dc2063c8aeb
web-intelligent <wwi19878@mail.ru>
2020-09-10 14:00:05 +0600

    Редактирование поста

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
                     </div>
                     <div class="posts">
                         <div class="row">
+                        <?php if( have_posts() ){while( have_posts() ){the_post();?>
                             <div class="col-sm-12 col-md-12 col-lg-6">
                                 <div class="post">
                                     <?php the_post_thumbnail(); ?>
@@ -24,15 +25,25 @@
                                     <p><?php the_excerpt();?></p>
                                     <div class="meta-info">
                                         <div class="posted-date-time">
-                                            <i class="far fa-calendar-alt"></i> 08.09.2020 <i class="far fa-clock"></i> 15:22
+                                            <i class="far fa-calendar-alt"></i> <?php the_date('d.m.Y'); ?> <i class="far fa-clock"></i> 15:22
                                         </div>
                                         <div class="author">
-                                            <i class="fas fa-user"></i> <?php echo the_author_firstname(  );?>
+                                            <i class="fas fa-user"></i> <?php the_author()?>
                                         </div>
                                     </div>
                                     <a href="#" class="more-info">Подробнее</a>
                                 </div>
                             </div>
+                            <?php }?>
+
+                            <div class="navigation">
+                                <div class="next-posts"><?php next_posts_link(); ?></div>
+                                <div class="prev-posts"><?php previous_posts_link(); ?></div>
+                            </div>
+
+                            <?php } else {
+                                echo "<h2>Записей нет.</h2>";
+                            }?>
                         </div>
                     </div>
                 </section>
